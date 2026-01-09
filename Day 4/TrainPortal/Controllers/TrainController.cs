ddcaf53c53064565fd8f76af911ed9626d2d810a
tahmidnoor <tahmidnoor.tn@gmail.com>
2026-01-09 14:57:59 +0530

    Day 4 Exercise Add

---
+++ Day 4/TrainPortal/Controllers/TrainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainPortal.Controllers
+{
+    public class TrainController : Controller
+    {
+        public IActionResult Courses()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
