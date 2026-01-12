e9b6acde4dad70aee47cffc067f8d311ee3f2f9d
tahmidnoor <tahmidnoor.tn@gmail.com>
2026-01-12 12:18:54 +0530

    Day 5 Exercise Add

---
+++ Day 5/GreetingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using GreetingLibrary;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine(Greet.Hello(userName));
+        }
+    }
+}
