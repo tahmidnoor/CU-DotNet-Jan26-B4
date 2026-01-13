379f92ce750694ff591b0cc757243d9d9ce54de0
tahmidnoor <tahmidnoor.tn@gmail.com>
2026-01-13 11:51:46 +0530

    Day 7 Exercise Add

---
+++ Day 7/README.md
@@ -0,0 +1,3 @@
+#SmartAccessControl
+
+![image alt](https://github.com/tahmidnoor/CU-DotNet-Jan26-B4/blob/6528b71d8547b7831668a49997bbc29d29bf66d8/Day%204/Screenshots/1.png)
---
+++ Day 7/SmartAccessControl/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace SmartAccessControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Entry Access Info.: ");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("INVALID ACCESS LOG!");
+                return;
+            }
+
+            string[] inputs = input.Split('|');
+
+            if (inputs.Length != 5)
+            {
+                Console.WriteLine("INVALID ACCESS LOG!");
+                return;
+            }
+
+            string gateCode = inputs[0];
+            if (gateCode.Length != 2 ||
+                !char.IsLetter(gateCode[0]) ||
+                !char.IsDigit(gateCode[1]))
+            {
+                Console.WriteLine("INVALID ACCESS LOG!");
+                return;
+            }
+
+            if (inputs[1].Length != 1)
+            {
+                Console.WriteLine("INVALID ACCESS LOG!");
+                return;
+            }
+
+            char userInitial = inputs[1][0];
+            if (!char.IsUpper(userInitial))
+            {
+                Console.WriteLine("INVALID ACCESS LOG!");
+                return;
+            }
+
+            if (!byte.TryParse(inputs[2], out byte accessLevel) ||
+                accessLevel < 1 || accessLevel > 7)
+            {
+                Console.WriteLine("INVALID ACCESS LOG!");
+                return;
+            }
+
+            if (!bool.TryParse(inputs[3], out bool isActive))
+            {
+                Console.WriteLine("INVALID ACCESS LOG!");
+                return;
+            }
+
+            if (!byte.TryParse(inputs[4], out byte attempts) ||
+                attempts > 200)
+            {
+                Console.WriteLine("INVALID ACCESS LOG!");
+                return;
+            }
+
+            string status;
+
+            if (!isActive)
+            {
+                status = "ACCESS DENIED – INACTIVE USER";
+            }
+            else if (attempts > 200)
+            {
+                status = "ACCESS DENIED – TOO MANY ATTEMPTS";
+            }
+            else if (accessLevel >= 5)
+            {
+                status = "ACCESS GRANTED – HIGH SECURITY";
+            }
+            else
+            {
+                status = "ACCESS GRANTED – STANDARD";
+            }
+
+            Console.WriteLine($"{"Gate",-10}: {gateCode}");
+            Console.WriteLine($"{"User",-10}: {userInitial}");
+            Console.WriteLine($"{"Level",-10}: {accessLevel}");
+            Console.WriteLine($"{"Attempts",-10}: {attempts}");
+            Console.WriteLine($"{"Status",-10}: {status}");
+        }
+    }
+}
