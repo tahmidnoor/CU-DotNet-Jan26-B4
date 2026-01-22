4b3ae55a4b3badfb47b905fef636c5228c99318e
tahmidnoor <tahmidnoor.tn@gmail.com>
2026-01-22 16:03:28 +0530

    Day 13 Exercise Add

---
+++ Day 13/LineMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LineMethod
+{
+    internal class Program
+    {
+        public static void Display(int num = 40, char ch = '-')
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Display():");
+            Display();
+            Console.WriteLine();
+            Console.WriteLine("Display(ch: '+'):");
+            Display(ch: '+');
+            Console.WriteLine();
+            Console.WriteLine("Display(60, '$'):");
+            Display(60, '$');
+
+        }
+    }
+}
