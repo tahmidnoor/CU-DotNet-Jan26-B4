21202e6776b3b167fe7eac626f8cf55d61e6d1d1
tahmidnoor <tahmidnoor.tn@gmail.com>
2026-01-22 16:07:17 +0530

    Day 14 Exercise Add

---
+++ Day 14/PersonClass/Person.cs
@@ -0,0 +1,64 @@
+﻿namespace PersonClass
+{
+    enum Departmant
+    {
+        Sales,
+        Accounts,
+        IT
+    }
+    internal class Person
+    {
+        private int Id;
+        public void SetId(int id)
+        {
+            this.Id = id;
+        }
+
+        public string Name { get; set; }
+
+        private Departmant department;
+
+        public Departmant Departmant
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if(value >= 50000 && value <= 90000)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Salary must be between the Specified Range!");
+                }
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"ID\t: {Id}");
+            Console.WriteLine($"Name\t: {Name}");
+            Console.WriteLine($"Dept.\t: {department}");
+            Console.WriteLine($"Salary\t: {salary}");
+        }
+        static void Main(string[] args)
+        {
+            Person emp = new Person();
+
+            emp.SetId(101);
+            emp.Name = "Tahmid Noor";
+            emp.Departmant = Departmant.IT;
+            emp.Salary = 80000;
+
+            emp.Display();
+        }
+    }
+}
