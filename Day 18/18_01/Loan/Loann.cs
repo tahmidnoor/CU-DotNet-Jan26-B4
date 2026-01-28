80f8fd0e7c50fbd1111a244173afe18c23554629
tahmidnoor <tahmidnoor.tn@gmail.com>
2026-01-28 16:06:12 +0530

    Day 18 Exercise Add

---
+++ Day 18/18_01/Loan/Loann.cs
@@ -0,0 +1,74 @@
+﻿namespace Loan
+{
+    internal class Loann
+    {
+        public string loanNo {  get; set; }
+        public string customerName { get; set; }
+        public decimal principalAmmount { get; set; }
+        public int years { get; set; }
+
+        public Loann()
+        {
+            loanNo = "L1";
+            customerName = string.Empty;
+            principalAmmount = 0;
+            years = 0;
+        } 
+        public Loann(string lnum, string cname, decimal pamm, int tiy)
+        {
+            loanNo = lnum;
+            customerName = cname;
+            principalAmmount = pamm;
+            years = tiy;
+        }
+
+        public double calculateEMI()
+        {
+            double SI = (double)(principalAmmount * 10 * years) / 100;
+            return SI;
+        }
+        static void Main(string[] args)
+        {
+            Loann[] loans = new Loann[4]
+            {
+                new HomeLoan("HL1", "Tahmid Noor", 700000, 10),
+                new HomeLoan("HL2", "Shreya Biswas", 500000, 8),
+                new CarLoan("CL1", "Aditi Chaturvedi", 800000, 5),
+                new CarLoan("CL2", "Shreyansh Vishnoi", 400000, 4)
+            };
+
+            foreach (var loan in loans)
+            {
+                Console.WriteLine($"[{loan.loanNo}] EMI: {loan.calculateEMI()}");
+            }
+        }
+    }
+
+    class HomeLoan : Loann
+    {
+        public HomeLoan() { }
+
+        public HomeLoan(string lnum, string cname, decimal pamm, int tiy) : base(lnum, cname, pamm, tiy) { }
+
+        public new double calculateEMI()
+        {
+            decimal processingFee = principalAmmount / 100;
+            double interest = (double)(principalAmmount * 8 * years) / 100;
+            return interest + (double)processingFee;
+        }
+    }
+
+    class CarLoan : Loann
+    {
+        public CarLoan() { }
+
+        public CarLoan(string lnum, string cname, decimal pamm, int tiy) : base(lnum, cname, pamm, tiy) { }
+
+        public new double calculateEMI()
+        {
+            principalAmmount += 15000;
+            double interest = (double)(principalAmmount * 9 * years) / 100;
+            return interest;
+        }
+    }
+}
---
+++ Day 18/18_02/EmployeeCompensation/Employee.cs
@@ -0,0 +1,81 @@
+﻿namespace EmployeeCompensation
+{
+    class PermanentEmployee : Employee
+    {
+        public PermanentEmployee(int eid, string ename, decimal bsal, int exp) : base(eid, ename, bsal, exp) { }
+
+        public new decimal calculateAnnualSalary()
+        {
+            decimal hra = 0.2m * basicSalary;
+            decimal allowance = 0.1m * basicSalary;
+            decimal loyaltyBonus = (expYears >= 5) ? 50000 : 0;
+            return base.calculateAnnualSalary() + hra + allowance + loyaltyBonus;
+        }
+    }
+
+    class ContractEmployee : Employee
+    {
+        public int conDur {  get; set; }
+        public ContractEmployee(int eid, string ename, decimal bsal, int exp, int dur) : base(eid, ename, bsal, exp)
+        {
+            conDur = dur;
+        }
+        
+        public new decimal calculateAnnualSalary()
+        {
+            decimal bonus = (conDur >= 12) ? 30000 : 0;
+            return base.calculateAnnualSalary() + bonus;
+        }
+    }
+
+    class InternEmployee : Employee
+    {
+        public InternEmployee(int eid, string ename, decimal bsal, int exp) : base(eid, ename, bsal, exp) { }
+
+        public new decimal calculateAnnualSalary()
+        {
+            return base.calculateAnnualSalary();
+        }
+    }
+
+    internal class Employee
+    {
+        public int empId {  get; set; }
+        public string empName { get; set; }
+        public decimal basicSalary { get; set; }
+        public int expYears { get; set; }
+        public Employee(int eid, string ename, decimal bsal, int exp)
+        {
+            empId = eid;
+            empName = ename;
+            basicSalary = bsal;
+            expYears = exp;
+        }
+        public decimal calculateAnnualSalary()
+        {
+            return basicSalary * 12;
+        }
+        public string displayEmp()
+        {
+            return $"Employee ID\t: {empId}\nEmployee Name\t: {empName}\nBasic Salary\t: {basicSalary}\nExperience\t: {expYears}\nAnnual Salary\t: {calculateAnnualSalary():F2}";
+        }
+        static void Main(string[] args)
+        {
+            Employee emp1 = new PermanentEmployee(101, "Tahmid", 30000, 6);
+            PermanentEmployee emp2 = new PermanentEmployee(102, "Shreya", 30000, 6);
+
+            Employee emp3 = new ContractEmployee(103, "Aditi", 25000, 2, 14);
+            Employee emp4 = new InternEmployee(104, "Tushar", 15000, 0);
+
+            Console.WriteLine("---- Employee Details ----\n");
+            Console.WriteLine(emp1.displayEmp());
+            Console.WriteLine($"In-Hand Salary\t: {emp1.calculateAnnualSalary():F2}\n");
+            Console.WriteLine(emp2.displayEmp());
+            Console.WriteLine($"In-Hand Salary\t: {emp2.calculateAnnualSalary():F2}\n");
+            Console.WriteLine(emp3.displayEmp());
+            Console.WriteLine($"In-Hand Salary\t: {emp3.calculateAnnualSalary():F2}\n");
+            Console.WriteLine(emp4.displayEmp());
+            Console.WriteLine($"In-Hand Salary\t: {emp4.calculateAnnualSalary():F2}\n");
+        }
+    }
+}
