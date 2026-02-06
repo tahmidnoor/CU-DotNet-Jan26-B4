6772cf5927a56a8262cb3e2671e288572ffae4ad
tahmidnoor <tahmidnoor.tn@gmail.com>
2026-02-06 11:50:27 +0530

    Day 27 Exercise Add

---
+++ Week 5/Day 27/27_01/DailyLogger/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DailyLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fl = "journal.txt";
+            string dir = "../../../";
+            string path = dir + fl;
+            Console.Write("Enter your Daily Reflection: ");
+            string refl = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine("Date: " + DateTime.Now);
+                writer.WriteLine(refl);
+                writer.WriteLine("---------------------");
+            }
+            Console.WriteLine("Reflection Saved!");
+        }
+    }
+}
