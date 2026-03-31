806401b8adadb9bdbdbc06cc47943cc1ad64fd5c
tahmidnoor <tahmidnoor.tn@gmail.com>
2026-03-31 11:55:01 +0530

    Week 1 Assessment

---
+++ Assessments/Week 1/GreetingLibrary/Greet.cs
@@ -0,0 +1,17 @@
+﻿namespace GreetingLibrary
+{
+    public class Greet
+    {
+        public static string Hello(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Hello, Guest!";
+            }
+            else
+            {
+                return $"Hello, {name}!";
+            }
+        }
+    }
+}
